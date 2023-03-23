bf8d1c0bb677a2bf3107250d0331c8c9a11ac22a
Simone Bauco <simonebauco1@gmail.com>
2023-03-23 18:41:17 +0100

    Date message added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // this is a comment
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        System.out.println("Today it's " + dtf.format(LocalDate.now()));
     }
 }
